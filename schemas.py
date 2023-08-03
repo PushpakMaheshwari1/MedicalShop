ad159cacdad206e889212d24c5126ce5d53d4146
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-08-03 20:48:59 +0530

    create schema for Cashbook Model

--- schemas.py
+++ schemas.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel
+from datetime import date
 
+
+class Cash_book(BaseModel):
+    id:int = None
+    sell_amount: float
+    date: date
+    profit_percentage: float
+    category_id : int
+
+    class Config:
+        from_attributes = True
 
 class Product_Category(BaseModel):
     id:int
     name:str
+
+    class Config:
+        from_attributes = True
