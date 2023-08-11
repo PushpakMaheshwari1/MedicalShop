84eda2748de116b6ca301bff7350efd49b6ee959
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-08-11 18:19:16 +0530

    Create Schema for tokens

--- schemas.py
+++ schemas.py
@@ -1,6 +1,27 @@
 from pydantic import BaseModel
 from datetime import date
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: int | None = None
+
+
+class Base_User(BaseModel):
+    username : str
+    email : str
+    password : str
+
+    class Config:
+        from_attributes = True
+
+
+class Login(BaseModel):
+    username:str
+    password:str
 
 class Cash_book(BaseModel):
     sell_amount: float
