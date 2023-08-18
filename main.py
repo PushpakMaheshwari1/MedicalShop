7a888514dce378b040480da79c3d323e18e78083
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-08-18 18:41:16 +0530

    Add pagination functionality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from auth_routes import auth_router
 from inventory_routes import inventory_router
+from fastapi_pagination import add_pagination
+
 
 app = FastAPI()
 
+add_pagination(app)
 app.include_router(auth_router)
 app.include_router(inventory_router)
